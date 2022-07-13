dae46c117465d1d5549df3e3931b55ae75e20471
LucaszLisboa <gabriel.lisboa.141@gmail.com>
2022-07-13 11:46:53 -0300

    GET api github

--- Aula08/script.js
+++ Aula08/script.js
@@ -1,18 +1,34 @@
-const data = { username: 'lucaszlisboa'};
+const mainEl = document.querySelector('#main');
 
 fetch('https://api.github.com/users/lucaszlisboa/repos', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-}) 
+        method: 'GET',
+        headers: {"Content-type": "application/json;charset=UTF-8"}
 
-.then(data => {
-    eu(data);
-    return data.JSON();
-})
+    }) 
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log('Erro de solicitação', err));
+  
 
-function eu(response){
-    console.log(response);
-}
+    function exibirRepos(response){
+        console.log(response);
+
+        JSON.stringify(response);
+
+        let name = document.createElement('div')
+        let linkName = document.createTextNode(response.html_url);
+        let link = document.createElement('a')
+        link.appendChild(linkName)
+
+        link.title = response.html_url;
+
+        name.classList.add('name')
+        link.classList.add('link')
+
+        name.innerText = response.name;
+        link.href = response.html_url
+        link.target = "_blank"
+
+        mainEl.appendChild(name);
+        mainEl.appendChild(link);
+    }
